21cc6a3263507b3c690be2b73afe014c83d1afe1
Adam Boudj <boudjemaa.adam@gmail.com>
2023-05-16 04:01:43 +0400

    🚑 fix missing contracts

---
+++ contracts/_testContracts/IdentityTrick.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.17;
+
+import "@onchain-id/solidity/contracts/Identity.sol";
+import "@onchain-id/solidity/contracts/ClaimIssuer.sol";
+
+abstract contract IdentityTrick is Identity, ClaimIssuer {}
